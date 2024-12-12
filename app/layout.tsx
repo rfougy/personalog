5148d49f4bc96963565061c8939b6b36b29738be
Riviere Fougy <rivierefougy@gmail.com>
2024-12-11 20:44:01 -0500

    refactor(style): nav and footer personalog branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,11 +36,9 @@ export default function RootLayout({
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={'/'}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
+                  <div className="flex gap-2 items-center font-semibold">
+                    <Link href={'/'}>PERSONALOG</Link>
+                    <ThemeSwitcher />
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
@@ -50,18 +48,7 @@ export default function RootLayout({
               </div>
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{' '}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
-                <ThemeSwitcher />
+                <p>Personalog, 2025</p>
               </footer>
             </div>
           </main>
