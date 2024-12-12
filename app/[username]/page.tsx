3e04e7fdb00852e1978ca5a5b204810cc519dd22
Riviere Fougy <rivierefougy@gmail.com>
2024-12-12 15:12:54 -0500

    feat: create user profile page

---
+++ app/[username]/page.tsx
@@ -0,0 +1,30 @@
+import { createClient } from '@/utils/supabase/server';
+
+interface UserProfileProps {
+  params: {
+    username: string;
+  };
+}
+
+export default async function UserProfile({ params }: UserProfileProps) {
+  const supabase = await createClient();
+
+  const { username } = params;
+
+  const { data: profile, error } = await supabase
+    .from('profiles')
+    .select('*')
+    .eq('username', username)
+    .single();
+
+  return error ? (
+    <div>
+      <h1>User not found</h1>
+      <p>{error.message}</p>
+    </div>
+  ) : (
+    <main className="min-h-screen flex flex-col gap-4">
+      <h2 className="font-bold">{profile.name.split(' ')[0]}'s Personalog</h2>
+    </main>
+  );
+}
