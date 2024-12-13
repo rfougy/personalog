5dc859e74677cd23ee2dba2a3f3b0c20e3cbd708
Riviere Fougy <rivierefougy@gmail.com>
2024-12-12 21:37:36 -0500

    refactor: create ErrorPage, cleanup NewPage

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,22 +1,25 @@
 import { createClient } from '@/utils/supabase/server';
 import { MarkdownEditor } from './components/markdown-editor';
+import ErrorPage from '@/components/error-page';
 
 export default async function NewPage() {
   const supabase = await createClient();
 
   const {
     data: { user },
-    error,
+    error: authError,
   } = await supabase.auth.getUser();
 
-  if (error) console.log(error);
+  if (authError) console.error('Error fetching user session:', authError);
 
-  const userId = user?.id;
+  const userId = user?.id || null;
 
-  return (
+  return userId ? (
     <main className="min-h-screen flex flex-col gap-4">
       <h2 className="font-bold">What's On Your Mind?</h2>
       <MarkdownEditor userId={userId} />
     </main>
+  ) : (
+    <ErrorPage />
   );
 }
---
+++ components/error-page.tsx
@@ -0,0 +1,8 @@
+export default function ErrorPage() {
+  return (
+    <main className="min-h-screen flex flex-col gap-4">
+      <h2 className="font-bold">Error Loading Page</h2>
+      <p>Please try again later.</p>
+    </main>
+  );
+}
