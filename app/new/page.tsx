7ef14a9bc9d0086d7de3764944f7675d0b002078
Riviere Fougy <rivierefougy@gmail.com>
2024-12-12 21:21:46 -0500

    chore(new): deconstruct user from data fetched

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -4,8 +4,14 @@ import { MarkdownEditor } from './components/markdown-editor';
 export default async function NewPage() {
   const supabase = await createClient();
 
-  const { data: session, error } = await supabase.auth.getUser();
-  const userId = session.user?.id;
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (error) console.log(error);
+
+  const userId = user?.id;
 
   return (
     <main className="min-h-screen flex flex-col gap-4">
