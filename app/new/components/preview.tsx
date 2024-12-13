da2abdc1e6c87ad5971215a3bfedee3aa9d36b16
Riviere Fougy <rivierefougy@gmail.com>
2024-12-12 21:43:45 -0500

    chore: uninstall react-syntax-highligher and related libraries, remoce code prop from Preview

--- app/new/components/preview.tsx
+++ app/new/components/preview.tsx
@@ -1,6 +1,4 @@
 import ReactMarkdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 interface PreviewProps {
   content: string;
@@ -23,23 +21,6 @@ export function Preview({ content }: PreviewProps) {
               >
                 {children}
               </a>
-            );
-          },
-          code({ node, inline, className, children, ...props }) {
-            const match = /language-(\w+)/.exec(className || '');
-            return !inline && match ? (
-              <SyntaxHighlighter
-                style={vscDarkPlus}
-                language={match[1]}
-                PreTag="div"
-                {...props}
-              >
-                {String(children).replace(/\n$/, '')}
-              </SyntaxHighlighter>
-            ) : (
-              <code className={className} {...props}>
-                {children}
-              </code>
             );
           },
         }}
